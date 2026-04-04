db8d7da7d84fecd2d3284a183f61742ebdc55e03
Mike Edmunds <medmunds@gmail.com>
2026-04-03 17:43:58 -0700

    Fixed #35514 -- Implemented dictionary-based EMAIL_PROVIDERS.
    Implemented `mail.providers` factory from `EMAIL_PROVIDERS` setting, and
    added `using` argument to mail sending APIs. New features run alongside
    existing functionality and settings (which will be deprecated in a later
    commit).
    
    Duplicated (via subclassing) most sending API tests in mail/tests.py to
    run them both with and without EMAIL_PROVIDERS defined.
    
    Removed some undocumented functionality:
    - Removed EmailMessage.get_connection() method. send() now raises an
      error if a subclass has attempted to override it.
    - EmailMessage.send() no longer sets self.connection to the connection
      used for sending. (It still _uses_ a pre-existing self.connection.)
    
    Co-authored-by: Jacob Rief <jacob.rief@gmail.com>

--- django/core/mail/message.py
+++ django/core/mail/message.py
@@ -305,13 +305,6 @@ class EmailMessage:
         self.extra_headers = headers or {}
         self.connection = connection
 
-    def get_connection(self, fail_silently=False):
-        from django.core.mail import get_connection
-
-        if not self.connection:
-            self.connection = get_connection(fail_silently=fail_silently)
-        return self.connection
-
     def message(self, *, policy=email.policy.default):
         msg = email.message.EmailMessage(policy=policy)
         self._add_bodies(msg)
@@ -349,20 +342,40 @@ class EmailMessage:
         """
         return [email for email in (self.to + self.cc + self.bcc) if email]
 
-    def send(self, fail_silently=False):
+    def send(self, fail_silently=False, *, using=None):
         """Send the email message."""
         if not self.recipients():
             # Don't bother creating the network connection if there's nobody to
             # send to.
             return 0
 
-        if fail_silently and self.connection:
-            raise TypeError(
-                "fail_silently cannot be used with a connection. "
-                "Pass fail_silently to get_connection() instead."
+        # RemovedInDjango70Warning: replace the remainder of this method with:
+        #   from django.core.mail import providers
+        #   provider = providers.default if using is None else providers[using]
+        #   return provider.send_messages([self])
+
+        from django.core import mail
+
+        if hasattr(self, "get_connection"):
+            raise AttributeError(
+                "EmailMessage no longer supports the undocumented "
+                "get_connection() method."
             )
 
-        return self.get_connection(fail_silently).send_messages([self])
+        if using is not None:
+            mail.incompatible_with_using(self.connection, fail_silently)
+            connection = mail.providers[using]
+        elif self.connection:
+            connection = self.connection
+            if fail_silently:
+                raise TypeError(
+                    "fail_silently cannot be used with a connection. "
+                    "Pass fail_silently to get_connection() instead."
+                )
+        else:
+            connection = mail.get_connection(fail_silently=fail_silently)
+
+        return connection.send_messages([self])
 
     def attach(self, filename=None, content=None, mimetype=None):
         """
