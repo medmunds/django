84bbac78ca2914c8b01226ff3c866213a1180baf
Mike Edmunds <medmunds@gmail.com>
2026-04-25 12:57:15 -0700

    [PR #21178: Added warn_about_external_use() deprecation utility.]

---
+++ tests/deprecation/internal.py
@@ -0,0 +1,70 @@
+# Simulated Django "internals" for WarnAboutExternalUseTests.
+#
+# Every function in this module ends up calling deprecated_function(), which
+# calls warn_about_external_use(). The other functions provide various stack
+# depths and qualnames for test purposes. All functions pass their arguments
+# through to warn_about_external_use().
+#
+# By default, this module is treated as part of Django internal code. Pass
+# internal_name_prefixes to override that.
+
+from django.utils.deprecation import (
+    RemovedAfterNextVersionWarning,
+    RemovedInNextVersionWarning,
+    deprecate_posargs,
+    warn_about_external_use,
+)
+
+
+def deprecated_function(message=None, category=None, **kwargs):
+    if "internal_name_prefixes" not in kwargs:
+        # Include this module in Django internals.
+        kwargs["internal_name_prefixes"] = ("django.", __name__)
+    elif kwargs["internal_name_prefixes"] is None:
+        # Use defaults.
+        del kwargs["internal_name_prefixes"]
+    warn_about_external_use(
+        message or "Message",
+        category or RemovedInNextVersionWarning,
+        **kwargs,
+    )
+
+
+def one_indirection(*args, **kwargs):
+    deprecated_function(*args, **kwargs)
+
+
+def two_indirections(*args, **kwargs):
+    one_indirection(*args, **kwargs)
+
+
+def three_indirections(*args, **kwargs):
+    two_indirections(*args, **kwargs)
+
+
+class Class:
+    def deprecated_method(self, *args, **kwargs):
+        deprecated_function(*args, **kwargs)
+
+    def one_indirection(self, *args, **kwargs):
+        self.deprecated_method(*args, **kwargs)
+
+    def two_indirections(self, *args, **kwargs):
+        self.one_indirection(*args, **kwargs)
+
+
+@deprecate_posargs(RemovedAfterNextVersionWarning, ["a"])
+def decorated(message=None, category=None, *, a=None, **kwargs):
+    deprecated_function(message, category, **kwargs)
+
+
+def call_decorated(*args, **kwargs):
+    decorated(*args, **kwargs)
+
+
+def nested(*args, **kwargs):
+    # inner.__qualname__ is something like "nested.<locals>.inner".
+    def inner(*args, **kwargs):
+        deprecated_function(*args, **kwargs)
+
+    inner(*args, **kwargs)
